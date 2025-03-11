9a9acf75507503f7e9f8ab45ee1cfa1693ec68ff
RonaldoMacedo <ronni29_100@hotmail.com>
2025-03-10 23:32:43 -0300

    Modificando a primeira tela

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 
@@ -13,9 +13,14 @@ public class Main extends Application {
 	@Override
 	public void start(Stage stage) {
 		try {
-			Parent parent = FXMLLoader.load(getClass().getResource("/gui/MainView.fxml"));
-			Scene scene = new Scene(parent);
-			stage.setScene(scene);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			ScrollPane scrollPane = loader.load();
+			
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+			
+			Scene mainScene = new Scene(scrollPane);
+			stage.setScene(mainScene);
 			stage.show();
 		} catch(IOException e) {
 			e.printStackTrace();
